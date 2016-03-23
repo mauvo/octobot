65ec297b2ca62107ee8e699d330290b457b8e424
David Pond <david.pond@red-gate.com>
2016-03-23 17:37:40 +0000

    Fixed call to process.uptime()

--- botkitBot.js
+++ botkitBot.js
@@ -10,6 +10,6 @@ var bot = controller.spawn({token: process.env.token}).startRTM();
 
 controller.hears(['uptime'], 'direct_message,direct_mention,mention', function (bot, message) 
     {
-        var uptime = timespan.parse(process.uptime * 1000);
+        var uptime = timespan.parse(process.uptime() * 1000);
 	    bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + '.');
     });
