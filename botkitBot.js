fadfc81446ce2e120dbce0ce2e87da9d8c55ca69
David Pond <davidpond@gmail.com>
2016-03-24 08:47:44 +0000

    Shorten "Uptime" response

--- botkitBot.js
+++ botkitBot.js
@@ -11,5 +11,5 @@ var bot = controller.spawn({token: process.env.token}).startRTM();
 controller.hears(['uptime'], 'direct_message,direct_mention,mention', function (bot, message) 
     {
         var uptime = timespan.parse(process.uptime() * 1000);
-	    bot.reply(message, ':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + '.');
+	    bot.reply(message, 'I have been running for ' + uptime + '.');
     });
